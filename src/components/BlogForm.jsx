b15845d53a15cff4fa5a53e2ca4422bd86bf5790
sunkyu_park <noncefaln@gmail.com>
2022-11-19 18:09:24 +0900

    유효성 검사 기능 추가

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -2,125 +2,154 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
 import { bool } from 'prop-types';
-import propTypes from 'prop-types'
-const BlogForm = ({editing}) => {
+import propTypes from 'prop-types';
+const BlogForm = ({ editing }) => {
   const navigate = useNavigate();
-  const {id} = useParams();
+  const { id } = useParams();
 
-  const [title, setTitle] = useState('')
-  const [originalTitle, setOriginalTitle] = useState('')
-  const [body, setBody] = useState('')
-  const [originalBody, setOriginalBody] = useState('')
-  const [publish, setPublish] = useState(false)
-  const [originalPublish, setOriginalPublish] = useState(false)
+  const [title, setTitle] = useState('');
+  const [originalTitle, setOriginalTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [originalBody, setOriginalBody] = useState('');
+  const [publish, setPublish] = useState(false);
+  const [originalPublish, setOriginalPublish] = useState(false);
+  const [titleError, setTitleError] = useState(false);
+  const [bodyError, setBodyError] = useState(false);
 
   useEffect(() => {
-    if(editing) {
-      axios.get(`ht tp://localhost:4000/posts/${id}`).then(response => {
-        setTitle(response.data.title)
-        setBody(response.data.body)
-        setOriginalTitle(response.data.title)
-        setOriginalBody(response.data.body)
-        setOriginalPublish(response.data.publish)
-      })    
+    if (editing) {
+      axios.get(`ht tp://localhost:4000/posts/${id}`).then((response) => {
+        setTitle(response.data.title);
+        setBody(response.data.body);
+        setOriginalTitle(response.data.title);
+        setOriginalBody(response.data.body);
+        setOriginalPublish(response.data.publish);
+      });
     }
   }, [id, editing]);
 
   const isEdited = () => {
-    return title !== originalTitle
-      || body !== originalBody
-      || publish !== originalPublish; 
-  }
+    return (
+      title !== originalTitle ||
+      body !== originalBody ||
+      publish !== originalPublish
+    );
+  };
 
   const goBack = () => {
-    if(editing) {
+    if (editing) {
       navigate(`/blogs/${id}`);
     } else {
-      navigate('/blogs')
+      navigate('/blogs');
     }
-  }
+  };
+
+  const validateForm = () => {
+    let validated = true;
+
+    if (title === '') {
+      setTitleError(true);
+      validated = false;
+    }
+
+    if (body === '') {
+      setBodyError(true);
+      validated = false;
+    }
+
+    return validated;
+  };
 
   const onSubmit = () => {
-    if(editing) {
-      axios.patch(`http://localhost:4000/posts/${id}`, {
-        title,
-        body,
-        publish
-      }).then(response => {
-        console.log(response) 
-        navigate(`/blogs/{id}`)
-      })
-    } else {
-      axios.post('http://localhost:4000/posts', {
-        title,
-        body,
-        publish,
-        createdAt : Date.now()
-      }).then(() => {
-        navigate('/blogs')
-      })
+    setTitleError(false);
+    setBodyError(false);
+    if (validateForm()) {
+      if (editing) {
+        axios
+          .patch(`http://localhost:4000/posts/${id}`, {
+            title,
+            body,
+            publish,
+          })
+          .then((response) => {
+            console.log(response);
+            navigate(`/blogs/{id}`);
+          });
+      } else {
+        axios
+          .post('http://localhost:4000/posts', {
+            title,
+            body,
+            publish,
+            createdAt: Date.now(),
+          })
+          .then(() => {
+            navigate('/blogs');
+          });
+      }
     }
-  }
+  };
 
   const onChangePublish = (e) => {
-    console.log(e.target.checked)
-    setPublish(e.target.checked)
-  }
-
+    console.log(e.target.checked);
+    setPublish(e.target.checked);
+  };
 
   return (
     <div>
       <h1>{editing ? 'Edit' : 'Create'} a blog post</h1>
       <div className='mb-3'>
         <label className='form-label'>Title</label>
-        <input 
-        className='form-control'
-        value={title}
-        onChange={(event) =>{setTitle(event.target.value);
-        }} /> 
+        <input
+          className={`form-control ${titleError ? 'border-danger' : ''}`}
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+        {titleError && <div className='text-danger'>Title is required</div>}
       </div>
       <div className='mb-3'>
         <label className='form-label'>Body</label>
-        <textarea 
-        className='form-control'
-        value={body}
-        onChange={(event) =>{setBody(event.target.value);
-        }}
-        rows='10' /> 
+        <textarea
+          className={`form-control ${bodyError ? 'border-danger' : ''}`}
+          value={body}
+          onChange={(event) => {
+            setBody(event.target.value);
+          }}
+          rows='10'
+        />
+        {bodyError && <div className='text-danger'>Body is required</div>}
       </div>
       <div className='form-check mb-3'>
-        <input className='form-check-input'
-        type='checkbox'
-        checked={publish}
-        onChange={onChangePublish}
-      />
-      <label className='form-check-label'>
-        Publish
-      </label>
+        <input
+          className='form-check-input'
+          type='checkbox'
+          checked={publish}
+          onChange={onChangePublish}
+        />
+        <label className='form-check-label'>Publish</label>
       </div>
-      <button 
+      <button
         className='btn btn-primary'
         onClick={onSubmit}
         disabled={editing && !isEdited()}
-        >
+      >
         {editing ? 'Edit' : 'Post'}
       </button>
-      <button 
-        className='btn btn-danger ms-2'
-        onClick={goBack}
-        >
+      <button className='btn btn-danger ms-2' onClick={goBack}>
         Cancel
       </button>
-      </div> 
-  )
-}
+    </div>
+  );
+};
 
 BlogForm.prototype = {
-  editing : propTypes.bool
-}
+  editing: propTypes.bool,
+};
 
 BlogForm.defaultProps = {
-  editing : false
-}
+  editing: false,
+};
 
-export default BlogForm
+export default BlogForm;
