ec6bed46b120204fe28dfc35a627dbff83936eb2
sunkyu_park <noncefaln@gmail.com>
2022-11-19 17:34:23 +0900

    검색 기능 추가

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,18 +1,18 @@
 import axios from 'axios';
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect, useCallback } from 'react';
 import Card from '../components/Card';
 import { useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import LoadingSpinner from '../components/LoadingSpinner';
 import Pagination from './Pagination';
 import { bool } from 'prop-types';
-import propTypes from 'prop-types'
+import propTypes from 'prop-types';
 
 const BlogList = ({ isAdmin }) => {
-  const navigate = useNavigate(); 
-  const location = useLocation(); 
-  const params = new URLSearchParams(location.search)
-  const pageParam = params.get('page')
+  const navigate = useNavigate();
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const pageParam = params.get('page');
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
@@ -23,111 +23,123 @@ const BlogList = ({ isAdmin }) => {
   const limit = 5;
 
   useEffect(() => {
-    setNumberOfPages(Math.ceil(setNumberOfPosts/limit));
+    setNumberOfPages(Math.ceil(setNumberOfPosts / limit));
   }, [numberOfPosts]);
 
   const onClickPageButton = (page) => {
-    navigate(`${location.pathname}?page=${page}`)
-  }
+    navigate(`${location.pathname}?page=${page}`);
+    setCurrentPage(page);
+    getPosts(page);
+  };
 
-  const getPosts = useCallback((page = 1) => {
-    let params = {
+  const getPosts = useCallback(
+    (page = 1) => {
+      let params = {
         _page: page,
         _limit: limit,
         _sort: 'id',
         _order: 'desc',
-        title_like: searchText
+        title_like: searchText,
+      };
+
+      useEffect(() => {
+        setCurrentPage(parseInt(pageParam) || 1);
+        getPosts(parseInt(pageParam) || 1);
+      }, []);
+
+      if (!isAdmin) {
+        params = { ...params, publish: true };
       }
-  
-  useEffect(() => {
-    setCurrentPage(parseInt(pageParam) || 1);
-    getPosts(parseInt(pageParam) || 1)
-  }, [pageParam])
+      axios
+        .get(`http://localhost:4000/posts`, {
+          params,
+        })
+        .then((res) => {
+          setNumberOfPosts(res.headers['x-total-count']);
+          setPosts(res.data);
+          setLoading(false);
+        });
+    },
+    [isAdmin, setSearchText]
+  );
 
-
-    if (!isAdmin) {
-      params = {... params, publish : true}
-    }
-    axios.get(`http://localhost:4000/posts`, {
-      params
-    }).then((res) => {
-      setNumberOfPosts(res.headers['x-total-count'])
-      setPosts(res.data);
-      setLoading(false);
-    })
-  }, [isAdmin, setSearchText])
-
-  const deleteBlog = (e, id)=> {
+  const deleteBlog = (e, id) => {
     e.stopPropagation();
 
     axios.delete(`http://localhost:4000/posts/${id}`).then(() => {
-      setPosts(prevPosts => prevPosts.filter(post =>  post.id !== id))
+      setPosts((prevPosts) => prevPosts.filter((post) => post.id !== id));
     });
+  };
+
+  if (loading) {
+    return <LoadingSpinner />;
   }
 
-  if (loading ) {
-      return (
-        <LoadingSpinner />
-      );
-    }
-
-<div>
-  BlogList
-  Pagination
-</div>
+  <div>BlogList Pagination</div>;
   const renderBlogList = () => {
-    return posts.map(post => {
-      return ( 
-          <Card key={post.id}
-                title={post.title}
-                onClick={() => navigate(`blogs/${post.id}`)}
-          >
-            {isAdmin ? (<div>
-              <button 
-              className='btn btn-danger btn-sm'
-              onClick={(e) => deleteBlog(e, post.id)}>
+    return posts.map((post) => {
+      return (
+        <Card
+          key={post.id}
+          title={post.title}
+          onClick={() => navigate(`blogs/${post.id}`)}
+        >
+          {isAdmin ? (
+            <div>
+              <button
+                className='btn btn-danger btn-sm'
+                onClick={(e) => deleteBlog(e, post.id)}
+              >
                 Delete
               </button>
-            </div>) : null}
-          </Card>
+            </div>
+          ) : null}
+        </Card>
       );
-    })
-  }
+    });
+  };
 
-  const onSearch = () => {
-    getPosts(1);
-  }
+  const onSearch = (e) => {
+    if (e.key === 'Enter') {
+      navigate(`${location.pathname}?page=1`);
+      setCurrentPage(1);
+      getPosts(1);
+    }
+  };
 
-    return (
-      <div>
-        <input
-          type="text"
-          placeholder='Search ..'
-          className='form-control'
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-          onKeyUp={onSearch}
-        />
-        {posts.length === 0 
-        ? <div>No blog posts found</div>
-        : <>
+  return (
+    <div>
+      <input
+        type='text'
+        placeholder='Search ..'
+        className='form-control'
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        onKeyUp={onSearch}
+      />
+      {posts.length === 0 ? (
+        <div>No blog posts found</div>
+      ) : (
+        <>
           {renderBlogList()}
-          {numberOfPages > 1 && <Pagination
-            currentPage={currentPage}
-            numberOfPages={numberOfPages}
-            onClick={onClickPageButton}
-          />}
-        </>}
-        
-      </div>
-    )
-}
+          {numberOfPages > 1 && (
+            <Pagination
+              currentPage={currentPage}
+              numberOfPages={numberOfPages}
+              onClick={onClickPageButton}
+            />
+          )}
+        </>
+      )}
+    </div>
+  );
+};
 
 BlogList.prototype = {
-  isAdmin : propTypes.bool
-}
+  isAdmin: propTypes.bool,
+};
 
 BlogList.defaultProps = {
-  isAdmin : false 
-}
-export default BlogList
+  isAdmin: false,
+};
+export default BlogList;
